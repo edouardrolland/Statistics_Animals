0ee3bda4709b636290382fd10f73901be0433ba1
edouardrolland <edouard.rlld@gmail.com>
2024-02-19 09:23:30 +0100

    Fixing the csv generation

--- generate_files.py
+++ generate_files.py
@@ -10,11 +10,11 @@ def extract_center_coordinates(annotations):
         bbox = annotation['bbox']
         x_center = bbox[0] + bbox[2] / 2
         y_center = bbox[1] + bbox[3] / 2
-        center_coordinates.append((x_center, y_center))
+        center_coordinates.append((x_center, y_center, annotation['category_id']))
     return center_coordinates
 
 
-def main():
+def main(plot_clusters=True):
     # Chemin vers le fichier JSON COCO
     json_file_path = '/home/edr/Desktop/Animal Herding Database/Goats-Geladas/kenyan-ungulates/ungulate-annotations/annotations-clean-name-pruned/annotations-clean-name-pruned.json'
     with open(json_file_path, 'r') as file:
@@ -22,26 +22,34 @@ def main():
 
     txt_file_path = f"animal_positions_global.csv"
     with open(txt_file_path, 'w') as csv_file:
-
-        csv_file.write(f"image name; centroids\n")
-
+        
+        csv_file.write('Animal Herd Position Database - Edouard George Alain Rolland - SDU Drone Center\n')
+        csv_file.write('categories":[{"id":1,"name":"zebra"},{"id":2,"name":"gazelle"},{"id":3,"name":"wbuck"},{"id":4,"name":"buffalo"},{"id":5,"name":"other"}]\n')
+        csv_file.write('image_file_name,x,y,category\n')
+        
         for image_data in data['images']:
 
-            x_centroid, y_centroid = [], []
             image_id = image_data['id']
             image_file_name = image_data['file_name']
             print(f"Processing image {image_file_name}...")
-            # Récupération des annotations pour cette image
+            
             image_annotations = [
                 annotation for annotation in data['annotations'] if annotation['image_id'] == image_id]
 
-            # Extraction des coordonnées du centre pour chaque annotation
             center_coordinates = extract_center_coordinates(image_annotations)
-            x_coords, y_coords = zip(*center_coordinates)
+            print(center_coordinates)
+            x_coords, y_coords, category = zip(*center_coordinates)
+            csv_file.write(f'{image_file_name},{center_coordinates}\n')
+        
 
-            # Normalize the x and y coordinates
-            normalized_x = [(x - min(x_coords)) / (max(x_coords) - min(x_coords)) for x in x_coords]
-            normalized_y = [(y - min(y_coords)) / (max(y_coords) - min(y_coords)) for y in y_coords]
+            try: 
+                # Normalize the x and y coordinates
+                normalized_x = [(x - min(x_coords)) / (max(x_coords) - min(x_coords)) for x in x_coords]
+                normalized_y = [(y - min(y_coords)) / (max(y_coords) - min(y_coords)) for y in y_coords]
+            
+            except: 
+        
+                None
             
             # Perform DBSCAN clustering
             eps = 0.2  # The maximum distance between two samples to be considered as neighbors
@@ -75,35 +83,44 @@ def main():
 
             cluster_sizes = [np.sum(labels == k) for k in unique_labels]
 
-            
-            # Plot histogram of cluster sizes
-            plt.figure()
-            plt.hist(cluster_sizes, bins='auto')
-            plt.xlabel('Cluster Size')
-            plt.ylabel('Frequency')
-            plt.title('Distribution of Cluster Sizes')
-            plt.show()
+            if plot_clusters == True:
+                # Plot histogram of cluster sizes
+                plt.figure()
+                plt.hist(cluster_sizes, bins='auto')
+                plt.xlabel('Cluster Size')
+                plt.ylabel('Frequency')
+                plt.title('Distribution of Cluster Sizes')
+                plt.show()
 
             # Perform Shapiro-Wilk normality test
-            _, p_value = stats.shapiro(cluster_sizes)
-            if p_value > 0.05:
-                print("Cluster sizes follow a Gaussian distribution.")
-            else:
-                print("Cluster sizes do not follow a Gaussian distribution.")
+            
+            try: 
+                _, p_value = stats.shapiro(cluster_sizes)
+                if p_value > 0.05:
+                    print("Cluster sizes follow a Gaussian distribution.")
+                else:
+                    print("Cluster sizes do not follow a Gaussian distribution.")
+                    
+        
 
-            # Print the mean, standard deviation, and minimum Mahalanobis distance to other clusters
-            mean_cluster_size = np.mean(cluster_sizes)
-            std_cluster_size = np.std(cluster_sizes)
-            min_distance = np.min([np.min(np.ma.masked_where(k == j, np.linalg.norm(xy - np.mean(xy, axis=0), axis=1))) for k in unique_labels for j in unique_labels if k != j])
+                # Print the mean, standard deviation, and minimum Mahalanobis distance to other clusters
+                mean_cluster_size = np.mean(cluster_sizes)
+                std_cluster_size = np.std(cluster_sizes)
+                min_distance = np.min([np.min(np.ma.masked_where(k == j, np.linalg.norm(xy - np.mean(xy, axis=0), axis=1))) for k in unique_labels for j in unique_labels if k != j])
 
-            print(f"Mean cluster size: {mean_cluster_size}")
-            print(f"Standard deviation of cluster size: {std_cluster_size}")
-            print(f"Minimum Mahalanobis distance to other clusters: {min_distance}")
-            plt.xlabel('Normalized X')
-            plt.ylabel('Normalized Y')
-            plt.title('DBSCAN Clustering')
-            #plt.show()
+                print(f"Mean cluster size: {mean_cluster_size}")
+                print(f"Standard deviation of cluster size: {std_cluster_size}")
+                print(f"Minimum Mahalanobis distance to other clusters: {min_distance}")
+                
+            except:
+                None
+                
+            if plot_clusters:
+                plt.xlabel('Normalized X')
+                plt.ylabel('Normalized Y')
+                plt.title('DBSCAN Clustering')
+                plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(False)
